8391dc268dde117bf123ef911a748bec72303256
adnan-sahin <adnansahin1@outlook.com>
2019-08-30 10:31:57 +0300

    To create vue datatable component and to bind headers and items to component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-data-table :items="flights" :headers="headers" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VDatatable from "./components/VDatatable.vue";
+import flights from "@/data/flights.json";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    "v-data-table": VDatatable
+  },
+  data() {
+    return {
+      flights: flights,
+      headers: [
+        "Date",
+        "Airline",
+        "Flight No",
+        "Origin",
+        "Schedule Time",
+        "Estimation Time",
+        "Note"
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/VDatatable.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <th v-for="(header,index) in headers" :key="index">{{header}}</th>
+      </thead>
+      <tbody>
+        <tr v-for="(item,index) in items" :key="index">
+          <td v-for="(value,index) in item" :key="index">{{value}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    headers: { required: true, type: Array },
+    items: { required: true, type: Array }
+  }
+};
+</script>
+<style scoped>
+</style>
